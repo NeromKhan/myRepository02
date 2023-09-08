ff9a433794216e227f38750ac6dfa7021517fa9e
NeromKhan <nerom.khan247@gmail.com>
2023-09-08 01:39:07 -0400

    added new class and added new line

---
+++ src/test/java/base/NewBaseClass.java
@@ -0,0 +1,8 @@
+package base;
+
+public class NewBaseClass {
+    public static void main(String[] args) {
+        System.out.println("my another priject");
+        //create another new line
+    }
+}
