2c9361386d2ba1658c234863183cc8315b5940ee
NeromKhan <nerom.khan247@gmail.com>
2023-09-24 14:02:07 -0400

    Uploeded a project

--- src/test/java/base/NewBaseClass.java
+++
@@ -1,8 +0,0 @@
-package base;
-
-public class NewBaseClass {
-    public static void main(String[] args) {
-        System.out.println("my another priject");
-        //create another new line
-    }
-}
